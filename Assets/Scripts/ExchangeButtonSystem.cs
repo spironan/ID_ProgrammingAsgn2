d97308037636bbfb25a80b9b791e03707937e3b8
spironan <C_tecklee@Hotmail.com>
2017-01-26 09:11:54 +0800

    added more unity stuff

---
+++ Assets/Scripts/ExchangeButtonSystem.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExchangeButtonSystem : MonoBehaviour {
+
+    public List<GameObject> scrollViewList = new List<GameObject>();
+    //For fading uses
+    private Color alphaColor;
+    public float timeToFade = 1.0f;
+
+    //to update
+    private GameObject currentObject;
+    private GameObject nextObject;
+
+    void Start()
+    {
+        currentObject = nextObject = null;
+
+        foreach (GameObject temp in scrollViewList)
+            temp.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (nextObject != currentObject)
+        {
+            ExitCurrent();
+            InitNext();
+            currentObject = nextObject;
+        }
+    }
+
+    private void ExitCurrent()
+    {
+        if (currentObject != null)
+        {
+            while (FadeOut(currentObject) > 0.0f)
+            { 
+            }
+            currentObject.SetActive(false);
+        }
+    }
+
+    private void InitNext()
+    {
+        if (nextObject != null)
+        {
+            while(FadeIn(nextObject) < 1.0f)
+            {
+            }
+            nextObject.SetActive(true);
+        }
+
+    }
+
+    private float FadeIn(GameObject fadingObject)
+    {
+        fadingObject.GetComponent<MeshRenderer>().material.color 
+            = Color.Lerp(fadingObject.GetComponent<MeshRenderer>().material.color, alphaColor, timeToFade * Time.deltaTime);
+
+        return fadingObject.GetComponent<MeshRenderer>().material.color.a;
+    }
+
+    private float FadeOut(GameObject fadingObject)
+    {
+        fadingObject.GetComponent<MeshRenderer>().material.color
+            = Color.Lerp(fadingObject.GetComponent<MeshRenderer>().material.color, alphaColor, timeToFade * Time.deltaTime);
+
+        return fadingObject.GetComponent<MeshRenderer>().material.color.a;
+    }
+
+
+    public void ChangeTab(string tabName)
+    {
+        foreach (GameObject temp in scrollViewList)
+        {
+            if (temp.name == tabName)
+                nextObject = temp;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/JoystickMovement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class JoystickMovement : MonoBehaviour ,IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image bgImg;
+    private Image joystickImg;
+    private Vector3 inputVector;
+
+    private void Start()
+    {
+        bgImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public virtual void OnDrag(PointerEventData data)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform
+            , data.position
+            , data.pressEventCamera
+            , out pos))
+        {
+            //Resize the position to 0 to 1
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector3(pos.x * 2 - 1, 0, pos.y * 2 - 1);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            //Move Joystick Img
+            joystickImg.rectTransform.anchoredPosition = 
+                new Vector3(inputVector.x * bgImg.rectTransform.sizeDelta.x / 3,
+                            inputVector.z * bgImg.rectTransform.sizeDelta.y / 3);
+        }
+    }
+
+    public virtual void OnPointerDown(PointerEventData data)
+    {
+        OnDrag(data);
+    }
+
+    public virtual void OnPointerUp(PointerEventData data)
+    {
+        inputVector = Vector2.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector2.zero;
+    }
+
+    public float Horizontal()
+    {
+        if (inputVector.x != 0)
+            return inputVector.x;
+        else
+            return Input.GetAxis("Horizontal");
+    }
+
+    public float Vertical()
+    {
+        if (inputVector.y != 0)
+            return inputVector.y;
+        else
+            return Input.GetAxis("Vertical");
+    }
+
+}
