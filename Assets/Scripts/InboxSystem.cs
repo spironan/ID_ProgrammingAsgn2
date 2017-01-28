08993a67f08a422131504f8bedacd20b76ce5ed0
andy <andy101@live.com.sg>
2017-01-28 10:38:12 +0800

    Incomplete Stuff

---
+++ Assets/Scripts/InboxSystem.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class InboxSystem : MonoBehaviour {
+
+    //// Use this for initialization
+    //void Start () {
+	
+    //}
+	
+    //// Update is called once per frame
+    //void Update () {
+	
+    //}
+    public Canvas MainCanvas;
+    public Canvas FPCanvas;
+    public Canvas ItemCanvas;
+    public Canvas GiftCanvas;
+
+    void Awake()
+    {
+        FPCanvas.enabled = true;
+        ItemCanvas.enabled = false;
+        GiftCanvas.enabled = false;
+    }
+
+    public void FPOn()
+    {
+        FPCanvas.enabled = true;
+        ItemCanvas.enabled = false;
+        GiftCanvas.enabled = false;
+    }
+
+    public void ItemOn()
+    {
+        ItemCanvas.enabled = true;
+        FPCanvas.enabled = false;
+        GiftCanvas.enabled = false;
+    }
+
+    public void GiftOn()
+    {
+        GiftCanvas.enabled = true;
+        FPCanvas.enabled = false;
+        ItemCanvas.enabled = false;
+    }
+
+    
+}
--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -7,5 +7,17 @@ public class SceneTransition : MonoBehaviour {
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                Application.Quit();
+            }
+        }
     }
 }
