a93f832627c79623de70032aa7a8c3c7bc48a7cf
spironan <C_tecklee@Hotmail.com>
2017-01-31 22:18:53 +0800

    edited scenes and added stuff

--- Assets/Scripts/FriendInviteSystem.cs
+++ Assets/Scripts/FriendInviteSystem.cs
@@ -7,71 +7,51 @@ public class FriendInviteSystem : MonoBehaviour {
 
     public List<Button> FriendButtons = new List<Button>();
 
-
     //to update
     private Button currentObject;
-    private Button nextObject;
 
     bool isPressed = false;
 
 	// Use this for initialization
 	void Start () {
-        currentObject = nextObject = null;
+        currentObject = null;
         foreach (Button temp in FriendButtons)
         {
             temp.interactable = true;
         }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        if (nextObject != currentObject)
-        {
-            ExitCurrent();
-            InitNext();
-            currentObject = nextObject;
-        }
-	}
-
-    private void InitNext()
-    {
-        if (nextObject != null)
-        {
-            nextObject.interactable = true;
-        }
-    }
-
-    private void ExitCurrent()
-    {
-        foreach(Button temp in FriendButtons)
-        {
-           if(temp != currentObject)
-           {
-               temp.interactable = false;
-               isPressed = true; 
-           }
-           
-        }
-    }
-
     public void ChangeButton(string buttonName)
     {
-        foreach (Button temp in FriendButtons)
+        //Check if any existing buttons is pressed
+        if (!isPressed)
         {
-            if (temp.name == buttonName)
-                nextObject = temp;
-        }
-        //if (isPressed)
-        //{
-            foreach(Button temp in FriendButtons)
+            foreach (Button temp in FriendButtons)
             {
-                if (isPressed && temp == nextObject)
+                if (temp.name == buttonName)
+                {
+                    currentObject = temp;
+                    currentObject.interactable = true;
+                }
+                else
+                {
+                    temp.interactable = false;
+                }
+            }
+            isPressed = true;
+        }
+        else // if button is pressed
+        {
+            //check if it is the same button that is pressed
+            if (currentObject.name == buttonName)
+            {
+                foreach (Button temp in FriendButtons)
                 {
                     temp.interactable = true;
-                    isPressed = false;
                 }
-                temp.interactable = true;
+                isPressed = false;
             }
+        }
     }
 
 }
